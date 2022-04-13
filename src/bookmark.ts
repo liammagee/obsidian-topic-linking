d24ea4d7d40daea0be446ea28ce57042310ca90d
liammagee <liam.magee@gmail.com>
2022-04-13 12:30:07 +1000

    Fix to bookmark downloads

--- src/bookmark.ts
+++ src/bookmark.ts
@@ -3,7 +3,8 @@ import {
     TFile, 
 	request,
     htmlToMarkdown,
-    normalizePath} from 'obsidian';
+    normalizePath,
+    debounce} from 'obsidian';
 import { TopicLinkingSettings } from './settings';
 
 
@@ -13,9 +14,9 @@ export class BookmarkContentExtractor {
     bookmarkPath: string;
 
     
-    async deleteBookmarks(vault: Vault) {
+    async deleteBookmarks(vault: Vault, bookmarkPath: string) {
         const filesToDelete: TFile[] = vault.getFiles().
-            filter((file : TFile) => file.path.indexOf(normalizePath(`${this.generatedPath}${this.bookmarkPath}`)) > -1 && file.extension === 'md');
+            filter((file : TFile) => file.path.indexOf(normalizePath(bookmarkPath)) > -1 && file.extension === 'md');
         for (let i = 0; i < filesToDelete.length; i++)
             await vault.delete(filesToDelete[i]);        
     }
@@ -26,11 +27,22 @@ export class BookmarkContentExtractor {
         this.bookmarkPath = settings.bookmarkPath;
 
         statusBarItemEl.setText('Retrieving web content as markdown...');
+        // Bookmark path
+        const bookmarkDir : string = `${this.generatedPath}${this.bookmarkPath}`;
 
         // If overwrite is enabled, delete all existing markdown files
         if (settings.bookmarkOverwrite) 
-            this.deleteBookmarks(vault);
+            this.deleteBookmarks(vault, bookmarkDir);
 
+        try {
+            await vault.createFolder(normalizePath(bookmarkDir));
+        }
+        catch (err) {
+            // Already exists? continue on
+        }
+
+        let headers : Record<string, string> = {};
+    
         // Get all files in the vault
         const files : TFile[] = vault.getMarkdownFiles().filter((file : TFile) => file.path.indexOf(this.bookmarkPath) === 0);
         for (let file of files) {
@@ -46,7 +58,8 @@ export class BookmarkContentExtractor {
                     try {
 
                         // Retrieve the contents of the link
-                        const htmlContents = await request({url: link});
+                        console.log(`Downloading content from ${link}`)
+                        let htmlContents = await request({url: link, headers: headers});
 
                         // Find the title, and override if not null
                         let title : string = link;
@@ -54,36 +67,41 @@ export class BookmarkContentExtractor {
                             const titleMatch = htmlContents.match(/<title>([^<]*)<\/title>/i);
                             if (titleMatch !== null && titleMatch.length > 1 && titleMatch[1] !== '') 
                                 title = titleMatch[1];
+
+                            // Ignore HTTP errors
+                            if (title.indexOf('40') === 0 || title.indexOf('50') === 0)
+                                continue;
+
+                            // Remove punctuation
+                            title = title.trim().replace(/[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,./:;<=>?@[\]^`{|}~·]/g, '-');
+
+                            // Remove trailing hyphens
+                            if (title.indexOf('-') === 0)
+                                title = title.substring(1);
+
+                            // Limit file name length
+                            title = title.substring(0, 50);
+
+                            // Add some unique identifier to the title
+                            title += `-${i}`;
+
+                            // Convert to Markdown and add link
+                            let md = htmlToMarkdown(htmlContents);
+                            md = `${link}\n\n${md}`;
+
+                            // Create the file
+                            const fileName: string = normalizePath(`${bookmarkDir}${title}.md`);
+                            console.log(`Writing content to ${fileName}`)
+
+                            const file = <TFile> vault.getAbstractFileByPath(fileName);
+                            if (file !== null) {
+                                if (settings.bookmarkOverwrite)
+                                    vault.modify(file, md);
+                            }
+                            else 
+                                vault.create(fileName, md);
                         }
-                            
 
-                        // Ignore HTTP errors
-                        if (title.indexOf('40') === 0 || title.indexOf('50') === 0)
-                            return;
-
-                        // Remove punctuation
-                        title = title.trim().replace(/[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,./:;<=>?@[\]^`{|}~·]/g, '-');
-
-                        // Remove trailing hyphens
-                        if (title.indexOf('-') === 0)
-                            title = title.substring(1);
-
-                        // Limit file name length
-                        title = title.substring(0, 50);
-
-                        // Convert to Markdown and add link
-                        let md = htmlToMarkdown(htmlContents);
-                        md = `${link}\n\n${md}`;
-
-                        // Create the file
-                        const fileName: string = normalizePath(`${this.generatedPath}${this.bookmarkPath}${title}.md`);
-                        const file = <TFile> vault.getAbstractFileByPath(fileName);
-                        if (file !== null) {
-                            if (settings.bookmarkOverwrite)
-                                vault.modify(file, md);
-                        }
-                        else 
-                            vault.create(fileName, md);
                     }
                     catch (err) {
                         console.log(err);
